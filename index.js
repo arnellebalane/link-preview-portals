2d71cf651319d3c5990a655e725e0d9abbe7e07e
Arnelle Balane <arnellebalane@gmail.com>
2019-05-17 22:07:51 +0800

    Feature-detect the portal element

--- index.js
+++ index.js
@@ -1,51 +1,56 @@
-let timeout;
+if ('HTMLPortalElement' in window) {
 
-document.addEventListener('mouseenter', e => {
-    if (e.target.matches && e.target.matches('a[href]')) {
-        const url = e.target.href;
-        if (url === location.href) {
-            return;
-        }
+    let timeout;
 
-        let portal = document.querySelector('portal#link-preview-portal');
-
-        if (portal) {
-            clearTimeout(timeout);
-            if (portal.src !== url) {
-                portal.src = url;
+    document.addEventListener('mouseenter', e => {
+        if (e.target.matches && e.target.matches('a[href]')) {
+            const url = e.target.href;
+            if (url === location.href) {
+                return;
             }
-        } else {
-            portal = document.createElement('portal');
-            portal.src = e.target.href;
-            portal.id = 'link-preview-portal';
 
-            document.body.appendChild(portal);
+            let portal = document.querySelector('portal#link-preview-portal');
+
+            if (portal) {
+                clearTimeout(timeout);
+                if (portal.src !== url) {
+                    portal.src = url;
+                }
+            } else {
+                portal = document.createElement('portal');
+                portal.src = e.target.href;
+                portal.id = 'link-preview-portal';
+
+                document.body.appendChild(portal);
+            }
         }
-    }
-}, {capture: true});
+    }, {capture: true});
 
-document.addEventListener('mouseleave', e => {
-    if (e.target.matches && e.target.matches('a[href]')) {
+    document.addEventListener('mouseleave', e => {
+        if (e.target.matches && e.target.matches('a[href]')) {
+            const portal = document.querySelector('portal#link-preview-portal');
+
+            if (portal) {
+                timeout = setTimeout(() => portal.remove(), 300);
+            }
+        }
+    }, {capture: true});
+
+    document.addEventListener('click', e => {
+        const link = e.target && e.target.closest('a[href]');
         const portal = document.querySelector('portal#link-preview-portal');
 
-        if (portal) {
-            timeout = setTimeout(() => portal.remove(), 300);
+        if (link && portal && portal.src === link.href) {
+            e.preventDefault();
+            portal.classList.add('activated');
+
+            portal.addEventListener('transitionend', e => {
+                if (e.propertyName === 'transform') {
+                    portal.activate();
+                }
+            }, {once: true});
         }
-    }
-}, {capture: true});
+    }, {capture: true});
 
-document.addEventListener('click', e => {
-    const link = e.target && e.target.closest('a[href]');
-    const portal = document.querySelector('portal#link-preview-portal');
 
-    if (link && portal && portal.src === link.href) {
-        e.preventDefault();
-        portal.classList.add('activated');
-
-        portal.addEventListener('transitionend', e => {
-            if (e.propertyName === 'transform') {
-                portal.activate();
-            }
-        }, {once: true});
-    }
-}, {capture: true});
+}
